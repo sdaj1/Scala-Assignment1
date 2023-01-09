d315ff0757ce3c7dd161a8112e826e42cb600d71
Md Shamshad Ansari <shamshad.ansari24@gmail.com>
2023-01-09 16:03:54 +0530

    update ITTEmployee

--- app/models/ITTEmployee.scala
+++ app/models/ITTEmployee.scala
@@ -3,22 +3,7 @@ package models
 import scala.util.control.Breaks
 import scala.util.control.Breaks.break
 
-class ITTEmployee extends ITT{
-  var emp:List[Employee]= ???
-  def deptEmpSize():Int= ???
 
-  def elder(): List[Any] = ???
-
-  def younger(): List[Any] = ???
-
-  def collectiveAge(): Int = ???
-
-  def averageAge(): Double = ???
-
-  def vacancies(): Int = ???
-
-  def accommodateAndPromotion(): List[String] = ???
-}
 object ITTEmployee extends App{
 
 
